5c2163b2fc53267cbb8bf4c439817b92c06dcf82
Nicolas Horta-Adam <nicolashortaadam@gmail.com>
2021-03-06 16:53:14 -0500

    setup eslint & prettier

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 140,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  tabWidth: 2,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
